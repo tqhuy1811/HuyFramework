f249945a43f27f5ad459b195189383c64cc62106
Huy <tqhuy1811@gmail.com>
2020-06-28 23:33:41 +0700

    minor refactor 2

--- Huy.Framework/Types/Enumeration.cs
+++ Huy.Framework/Types/Enumeration.cs
@@ -112,18 +112,4 @@ namespace Huy.Framework.Types
 			return Value.CompareTo(((Enumeration) other).Value);
 		}
 	}
-	// Sample
-	
-	public class EmployeeType : Enumeration
-	{
-		public static readonly EmployeeType Manager 
-			= new EmployeeType(0, "Manager");
-		public static readonly EmployeeType Servant 
-			= new EmployeeType(1, "Servant");
-		public static readonly EmployeeType AssistantToTheRegionalManager 
-			= new EmployeeType(2, "Assistant to the Regional Manager");
-
-		private EmployeeType() { }
-		private EmployeeType(int value, string displayName) : base(value, displayName) { }
-	}
 }
