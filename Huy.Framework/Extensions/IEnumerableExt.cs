3ae69a0d49c04301ceb07f111c09ebb4f5fb8169
Huy <tqhuy1811@gmail.com>
2020-06-28 23:33:16 +0700

    minor refactor

--- Huy.Framework/Extensions/IEnumerableExt.cs
+++ Huy.Framework/Extensions/IEnumerableExt.cs
@@ -1,27 +1,24 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using Huy.Framework.Types;
 using static Huy.Framework.Types.UnitExt;
 
 namespace Huy.Framework.Extensions
 {
-	public static class IEnumerableExt
+	public static class EnumerableExt
 	{
 		public static IEnumerable<(int index, T data)> WithIndex<T>(this IEnumerable<T> @this)
 		{
-			return @this.Select((_, i) => (i, _));
+			return @this.Select((_,
+				i) => (i, _));
 		}
 
 		public static Unit ForEach<T>(
 			this IEnumerable<T> @this,
 			Action<T> func)
 		{
-			foreach (var item in @this)
-			{
-				func(item);
-			}
+			foreach (var item in @this) func(item);
 
 			return Unit();
 		}
